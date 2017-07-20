a0d64ed957e7bbd9e3da97224566b5aacbb543ae
Andrey A. Konovalov <drvtiny@gmail.com>
2017-07-20 18:14:57 +0300

    log_open and log_level added

--- KISS.pm
+++ KISS.pm
@@ -17,27 +17,73 @@ my %logMethodDefs=(
 my @logMethods=keys %logMethodDefs;
 my %logSev2N=do {
     no strict 'refs';
-    map {my $U=uc($_); $_=>$$U } @logSevs;
+    map {my $U=uc($_); ($_=>$$U, uc($_)=>$$U) } @logSevs;
 };
 
-our @EXPORT=our @EXPORT_OK=(map { $_.'_', $_ } @logSevs, @logMethods);
+our @EXPORT=our @EXPORT_OK=(qw/log_open log_level/, map { $_.'_', $_ } @logSevs, @logMethods);
 
 Log::Log4perl->wrapper_register(__PACKAGE__);
+my %fileOpenModes=(
+    '>' => 'clobber',
+    '>>' => 'append',
+    'write' => 'clobber',
+    '|-' => 'pipe',
+    'append' => 'append',
+    'clobber' => 'clobber',
+    'pipe' => 'pipe'
+);
+
+my $useAppender = 'Screen';
+my $layPattern = q(%d{HH:mm:ss} | %d{dd.MM.yyyy} | %P | %p | %m%n);
+my $layPackage ='Log::Log4perl::Layout::PatternLayout';
+my $logLevel   = 'DEBUG';
+my %L4PAppenders=(
+    'Screen' => {
+        'stderr' 		=> 1,
+        'layout'                => $layPackage,
+        'layout.ConversionPattern' => $layPattern
+    },
+    'File' => {
+        'filename' 		=> undef,
+        'mode'			=> undef,
+        'layout'		=> $layPackage,
+        'layout.ConversionPattern' => $layPattern,
+        'recreate'		=> 1,
+        'recreate_check_signal' => 'XFSZ',
+        'utf8'			=> 1
+    },
+);
+
+sub log_open {
+    my ($mode, $filePath)=
+        (@_==2) ? @_ : (@_==1) ? ('append', $_[0]) : confess 'Wrong number of arguments';
+    confess 'Invalid log open mode specified: '.$mode if $mode and !($mode=$fileOpenModes{lc $mode});
+    @{$L4PAppenders{$useAppender='File'}}{qw/filename mode/}=($filePath,$mode)
+}
+
+sub log_level {
+    confess 'Unknown log level specified' unless !ref($_[0]) and $_[0] and exists $logSev2N{lc $_[0]};
+    $logLevel=uc($_[0])
+}
+
+sub getL4PConfig {
+    my ($appndrType, $appndrConfig)=@_;
+    my $ptrn="log4perl.appender.${appndrType}";
+    my $l4pConf=join("\n" =>
+        "log4perl.rootLogger=${logLevel},${appndrType}"	,
+        "${ptrn}=Log::Log4perl::Appender::${appndrType}",
+        map sprintf("${ptrn}.%s=%s", each $appndrConfig->{$appndrType}),
+                1..keys($appndrConfig->{$appndrType})
+    );
+    return \$l4pConf
+}        
 
 my %pkgLoggers;
 sub log_ {
-    state $dfltL4PConf=\<<'EOLOGCONF';
-log4perl.rootLogger     =       DEBUG, Screen
-
-log4perl.appender.Screen = Log::Log4perl::Appender::Screen
-log4perl.appender.Screen.stderr = 1
-log4perl.appender.Screen.layout = Log::Log4perl::Layout::PatternLayout
-log4perl.appender.Screen.layout.ConversionPattern = %d{HH:mm:ss} | %d{dd.MM.yyyy} | %P | %p | %m%n
-EOLOGCONF
     return unless my $logSev=shift;
     my $logger=$pkgLoggers{scalar caller(1)}||=(
                 Log::Log4perl->initialized() 
-                 || Log::Log4perl->init($dfltL4PConf)
+                 || Log::Log4perl->init(getL4PConfig($useAppender, \%L4PAppenders))
                ) && Log::Log4perl->get_logger() 
                      or return;
     my ($doBeforeLog,$doAfterLog);
